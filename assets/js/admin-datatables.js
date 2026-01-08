2cc00d0a039bdf4fba37ae61e91427eb94dfc685
unknown <mahbub.progressivebyte@gmail.com>
2026-01-08 10:16:17 +0600

    data table issue solved

--- assets/js/admin-datatables.js
+++ assets/js/admin-datatables.js
@@ -2,94 +2,106 @@ jQuery(document).ready(function($) {
     
     // Initialize DataTables for Authors table
     if ($('#authors-table').length) {
-        $('#authors-table').DataTable({
-            responsive: true,
-            pageLength: 25,
-            lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
-            order: [[2, 'asc']], // Sort by Name column (changed from 1 to 2)
-            columnDefs: [
-                { 
-                    targets: -1, // Actions column
-                    orderable: false,
-                    searchable: false
+        // Check if table has data rows
+        var hasData = $('#authors-table tbody tr td').length > 0 && 
+                      !$('#authors-table tbody tr td').first().attr('colspan');
+        
+        if (hasData) {
+            $('#authors-table').DataTable({
+                responsive: true,
+                pageLength: 25,
+                lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+                order: [[2, 'asc']], // Sort by Name column
+                columnDefs: [
+                    { 
+                        targets: -1, // Actions column
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        targets: 0, // ID column
+                        width: '60px'
+                    },
+                    {
+                        targets: 1, // Image column
+                        width: '80px',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        targets: 2, // Name column
+                        width: '200px'
+                    }
+                ],
+                language: {
+                    search: "Search Authors:",
+                    lengthMenu: "Show _MENU_ authors per page",
+                    info: "Showing _START_ to _END_ of _TOTAL_ authors",
+                    infoEmpty: "No authors found",
+                    infoFiltered: "(filtered from _MAX_ total authors)",
+                    zeroRecords: "No matching authors found",
+                    emptyTable: "No authors available",
+                    paginate: {
+                        first: "First",
+                        last: "Last",
+                        next: "Next",
+                        previous: "Previous"
+                    }
                 },
-                {
-                    targets: 0, // ID column
-                    width: '60px'
-                },
-                {
-                    targets: 1, // Image column (NEW)
-                    width: '80px',
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    targets: 2, // Name column (changed from 1 to 2)
-                    width: '200px'
-                }
-            ],
-            language: {
-                search: "Search Authors:",
-                lengthMenu: "Show _MENU_ authors per page",
-                info: "Showing _START_ to _END_ of _TOTAL_ authors",
-                infoEmpty: "No authors found",
-                infoFiltered: "(filtered from _MAX_ total authors)",
-                zeroRecords: "No matching authors found",
-                emptyTable: "No authors available",
-                paginate: {
-                    first: "First",
-                    last: "Last",
-                    next: "Next",
-                    previous: "Previous"
-                }
-            },
-            dom: '<"top"lf>rt<"bottom"ip><"clear">'
-        });
+                dom: '<"top"lf>rt<"bottom"ip><"clear">'
+            });
+        }
     }
 
     // Initialize DataTables for Publishers table
     if ($('#publishers-table').length) {
-        $('#publishers-table').DataTable({
-            responsive: true,
-            pageLength: 25,
-            lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
-            order: [[1, 'asc']], // Sort by Name column
-            columnDefs: [
-                { 
-                    targets: -1, // Actions column
-                    orderable: false,
-                    searchable: false
+        // Check if table has data rows
+        var hasData = $('#publishers-table tbody tr td').length > 0 && 
+                      !$('#publishers-table tbody tr td').first().attr('colspan');
+        
+        if (hasData) {
+            $('#publishers-table').DataTable({
+                responsive: true,
+                pageLength: 25,
+                lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+                order: [[1, 'asc']], // Sort by Name column
+                columnDefs: [
+                    { 
+                        targets: -1, // Actions column
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        targets: 0, // ID column
+                        width: '60px'
+                    },
+                    {
+                        targets: 1, // Name column
+                        width: '200px'
+                    },
+                    {
+                        targets: 3, // Website column
+                        width: '250px'
+                    }
+                ],
+                language: {
+                    search: "Search Publishers:",
+                    lengthMenu: "Show _MENU_ publishers per page",
+                    info: "Showing _START_ to _END_ of _TOTAL_ publishers",
+                    infoEmpty: "No publishers found",
+                    infoFiltered: "(filtered from _MAX_ total publishers)",
+                    zeroRecords: "No matching publishers found",
+                    emptyTable: "No publishers available",
+                    paginate: {
+                        first: "First",
+                        last: "Last",
+                        next: "Next",
+                        previous: "Previous"
+                    }
                 },
-                {
-                    targets: 0, // ID column
-                    width: '60px'
-                },
-                {
-                    targets: 1, // Name column
-                    width: '200px'
-                },
-                {
-                    targets: 3, // Website column
-                    width: '250px'
-                }
-            ],
-            language: {
-                search: "Search Publishers:",
-                lengthMenu: "Show _MENU_ publishers per page",
-                info: "Showing _START_ to _END_ of _TOTAL_ publishers",
-                infoEmpty: "No publishers found",
-                infoFiltered: "(filtered from _MAX_ total publishers)",
-                zeroRecords: "No matching publishers found",
-                emptyTable: "No publishers available",
-                paginate: {
-                    first: "First",
-                    last: "Last",
-                    next: "Next",
-                    previous: "Previous"
-                }
-            },
-            dom: '<"top"lf>rt<"bottom"ip><"clear">'
-        });
+                dom: '<"top"lf>rt<"bottom"ip><"clear">'
+            });
+        }
     }
 
     // Optional: Add custom styling for better WordPress integration
